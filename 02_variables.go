e08a9c9d5a6100e6c5efa1fc45396f0bfed45cee
Yetty2020 <fatihaadetoro@gmail.com>
2026-09-11 22:04:20 +0100

    feat: Different variable types

--- 02_variables.go
+++ 02_variables.go
@@ -3,8 +3,16 @@ package main
 import "fmt"
 
 func main() {
+	//setting different variable types
+	var name string
+	name = "Fatihah"
+	var isTrue bool
+	isTrue = true
+	var floatNum float64
+	floatNum = 3.14
 	var newAge int
 	newAge = 30
 	fmt.Println("I just clocked", newAge)
+	fmt.Println("I am", name, "and I am", floatNum, "years old.", "This information is", isTrue)
 
 }
